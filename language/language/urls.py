2e5d3f715a0d4ee5137dbc50e618ee789534db62
m516 <m516@m516>
2015-10-24 01:38:45 +0800

    today

--- language/language/urls.py
+++ language/language/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
- 
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^main/', include('main.urls', namespace='main')),
     url(r'^wiki/', include('wiki.urls', namespace='wiki')),
 
